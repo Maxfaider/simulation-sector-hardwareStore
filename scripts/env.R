1353bd910f2536174c9279fa0b9983a25971b32d
Alan M.E <alanmarquez@outlook.com>
2018-06-09 00:32:28 -0500

    Evaluacion de datos & preparación del entorno

---
+++ scripts/env.R
@@ -0,0 +1,13 @@
+#variables entorno
+
+#Description:
+#
+#
+
+env <- getwd()
+
+env.data <- paste(env, "data", sep="/")
+env.annotations <- paste(env, "data", sep="/")
+env.graphics <- paste(env, "data", sep="/")
+env.scripts <- paste(env, "data", sep="/")
+env.stories <- paste(env, "data", sep="/")
---
+++ scripts/project.R
@@ -0,0 +1,12 @@
+#Información del proyecto
+
+# Description:
+#
+#
+
+project.name <- "simulation-sector-hardwareStore"
+project.version <- "1.0"
+project.author <- c(
+    "Alan M.E - <alanmarquez@outlook.com>",
+    "Rafael C.D <cum@gamil.com>"
+)
